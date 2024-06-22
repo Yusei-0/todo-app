8adb782aaa445ad3f668ff645366b1050aaf4ca5
Juseph Ortiz <juseph@ingeniuscuba.com>
2024-06-22 13:11:35 -0400

    add icon library

--- src/app/base/icon/icon.component.ts
+++ src/app/base/icon/icon.component.ts
@@ -1,14 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, input } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  saxAddCircleOutline,
+  saxAddOutline,
+  saxCalendar1Outline,
+  saxCalendarAddOutline,
+  saxCalendarEditOutline,
+  saxClockOutline,
+  saxMenuOutline,
+  saxNote1Outline,
+  saxNote21Outline,
+  saxNoteAddOutline,
+  saxNotificationBingOutline,
+  saxTimer1Outline,
+  saxTimerPauseOutline,
+  saxHashtagOutline,
+  saxMore2Outline,
+  saxMoreOutline,
+  saxTaskOutline,
+  saxTaskSquareOutline,
+  saxTickCircleOutline,
+  saxRecordCircleOutline,
+} from '@ng-icons/iconsax/outline';
+
+export type Icon =
+  | 'saxTimer1Outline'
+  | 'saxTimerPauseOutline'
+  | 'saxClockOutline'
+  | 'saxNote21Outline'
+  | 'saxNote1Outline'
+  | 'saxNoteAddOutline'
+  | 'saxCalendar1Outline'
+  | 'saxCalendarAddOutline'
+  | 'saxCalendarEditOutline'
+  | 'saxAddOutline'
+  | 'saxAddCircleOutline'
+  | 'saxMenuOutline'
+  | 'saxNotificationBingOutline'
+  | 'saxHashtagOutline'
+  | 'saxMore2Outline'
+  | 'saxMoreOutline'
+  | 'saxTaskOutline'
+  | 'saxTaskSquareOutline'
+  | 'saxTickCircleOutline'
+  | 'saxRecordCircleOutline';
 
 @Component({
-  selector: 'app-icon',
+  selector: 'icon',
   templateUrl: './icon.component.html',
   styleUrls: ['./icon.component.scss'],
+  imports: [NgIconComponent],
+  viewProviders: [
+    provideIcons({
+      saxTimer1Outline,
+      saxTimerPauseOutline,
+      saxClockOutline,
+      saxNote21Outline,
+      saxNote1Outline,
+      saxNoteAddOutline,
+      saxCalendar1Outline,
+      saxCalendarAddOutline,
+      saxCalendarEditOutline,
+      saxAddOutline,
+      saxAddCircleOutline,
+      saxMenuOutline,
+      saxNotificationBingOutline,
+      saxHashtagOutline,
+      saxMore2Outline,
+      saxMoreOutline,
+      saxTaskOutline,
+      saxTaskSquareOutline,
+      saxTickCircleOutline,
+      saxRecordCircleOutline
+    }),
+  ],
+  standalone: true,
 })
-export class IconComponent  implements OnInit {
+export class IconComponent implements OnInit {
 
-  constructor() { }
+  name = input.required<Icon>();
+
+  constructor() {}
 
   ngOnInit() {}
-
 }
